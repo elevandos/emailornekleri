72288849970cd968e09f9b9dc044599fc3f6f8a6
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-03-05 02:01:19 +0300

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css"
 const Footer = ()=>{
 return (<footer className={styles.footer}>
 <div className={styles.container}>
-<p className={styles.copy}>Powered By <b><a href="https://openai.com/">OpenAI</a></b> | &copy; <a href="https://flerosoft.com">Flerosoft</a></p>
+<p className={styles.copy}>Powered By <b><a href="https://openai.com/">OpenAI</a></b> | &copy; <a href="https://elevandos.com">Elevandos</a></p>
 </div>
 </footer>)
 }
