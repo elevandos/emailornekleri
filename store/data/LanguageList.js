3c78cb98cdbc5004c5f662acb2dffda7c9510e93
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-03-05 02:04:35 +0300

    Update LanguageList.js

--- store/data/LanguageList.js
+++ store/data/LanguageList.js
@@ -9,7 +9,6 @@ export const LANGUAGE_LIST = [
   // { name: "Nederlands", code: "nl" },
   // { name: "Svenska", code: "sv" },
   // { name: "Polski", code: "pl" },
- 
   // { name: "Русский", code: "ru" },
   // { name: "日本語", code: "ja" },
   // { name: "中文", code: "zh" },
