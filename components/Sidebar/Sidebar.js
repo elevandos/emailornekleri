9ee339399cff0d841256c3de107998cc3a033553
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-03-05 15:00:27 +0300

    Update Sidebar.js

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = () => {
               <img src="assets/images/banner-300x250.png" />
             </div>
             <div className={'ads banner-300x250'}>
-              <img src="assets/images/banner-300x600.png" />
+              <img src="assets/images/elevandos-300x600.png" />
             </div>
             <div className={'ads sticky banner-300x250'}>
               <img src="assets/images/banner-300x250.png" />
