98ae630beaef6544bb86599d55878de57b53cb68
Elevandos <126990491+elevandos@users.noreply.github.com>
2023-03-05 15:10:14 +0300

    Update DetailsSection.js

--- components/DetailsSection/DetailsSection.js
+++ components/DetailsSection/DetailsSection.js
@@ -13,19 +13,19 @@ const DetailsSection = () => {
     return (
         <div className={styles['details-wrapper']}>
             <div className={'ads ad-desktop banner-728x90'}>
-              <img src="assets/images/elevandos-728x90.png" />
+              <img src="assets/images/banner-728x90.png" />
             </div>
             <div className={'ads ad-mob banner-300x250'}>
               <img src="assets/images/banner-300x250.png" />
             </div>
             <div className={styles['info-box']}>
                 <div className={styles['head']}>
-                    <h2 className={styles['title']}Email GPT Nedir?</h2>
+                    <h2 className={styles['title']}>What is EmailGPT?</h2>
                 </div>
                 <div id="infoBody" className={styles['body']}>
-                    <p className={styles.para}>EmailGPT, bir kurumsal e-posta veya bir satış & pazarlama emaili oluşturmak için OpenAI desteği ile işlerinizi hızlandırmak için tasarlandı"."işiniz için değil günlük yaşamınızda da gönderebileceğiniz onlarca e-posta şablonuna saniyeler içerisinde erişin</p>
-                    <p className={styles.para}>Dil veya çeviri sorunu yaşayanlar için İngilizce e-posta örnekleri veya Almanca e-posta örnekleri gibi bir çok dilde hazır e-posta şablonları ücretsiz olarak sunulmaktadır "." Emailorneklericom da işinizi kolaylaştıracak ücretsiz e-posta şablonlarına kolayca erişin. </p>
-                    <p className={styles.para}>Yapay zekanın yaşamımızdaki yerine, bizzat bizim de deneyimlediğimiz kurumsal e-posta oluşturma sorununa çözüm oluşturduk"."Yeni bir satış temsilcisi veya kurumsal hayata yeni başlamış bir stajyer iseniz sizin için oldukça faydalı bir araç olacaktır. </p>
+                    <p className={styles.para}>EmailGPT is a revolutionary new web app that leverages the power of artificial intelligence to create high-quality emails with ease. Whether you're a busy professional, a student, or anyone in between, EmailGPT can help you streamline your email communication and improve the overall quality of your messages. With its cutting-edge tech stack featuring ReactJS, NextJS, NPM, and the OpenAI API, EmailGPT is the ultimate tool for anyone looking to take their email communication to the next level.</p>
+                    <p className={styles.para}>With EmailGPT, you no longer need to spend hours drafting emails or struggling with writer's block. Simply enter your desired subject or topic, and let the AI-powered tool generate a natural-sounding email for you in seconds. Whether you're crafting a job application, a thank-you note, or a simple message to a colleague, EmailGPT can help you craft emails that are tailored to your recipient's needs and preferences.</p>
+                    <p className={styles.para}>So why wait? Try EmailGPT today and experience the power of AI-powered email generation for yourself. With its user-friendly interface, customization options, and time-saving efficiency, EmailGPT is the ultimate tool for anyone looking to streamline their email communication and improve the overall quality of their messages.</p>
                 </div>
             </div>
             <div className={'ads ad-desktop banner-728x90'}>
@@ -36,19 +36,19 @@ const DetailsSection = () => {
             </div>
             <div className={styles['info-box']}>
                 <div className={styles['head']}>
-                    <h2 className={styles['title']}>Email GPT yani Emailornekleri.com nasıl çalışır?</h2>
+                    <h2 className={styles['title']}>How Does EmailGPT Work?</h2>
                 </div>
                 <div id="infoBody" className={styles['body']}>
-                    <p className={styles.para}>Saniyeler içerisinde sizin için email örnekleri oluşturabilen Open AI destekli emailornekleri com un kullanımı çok basittir. İşte adım adım Email Örnekleri uygulamasının kullanımı</p>
+                    <p className={styles.para}>Using EmailGPT is easy and intuitive. Simply enter your desired subject or topic, and let the AI-powered tool generate a high-quality email for you in seconds. Here's a step-by-step guide to using EmailGPT:</p>
                     <ol className={styles.list}>
-                        <li>Email Örnekleri web sitesine telefon veya bilgisayarınızdan giriş yapın.</li>
-                        <li>Hangi dilde sonuç almak istiyorsanız yukarıdan dil seçimini yapın.</li>
-                        <li>Sayfanın ortasında yer alan arama alanına E-posta konusu veya içeriğine dair bir kelime yazın.</li>
-                        <li>Karşınıza çıkan konulardan size en uygun olanı seçerek "E-posta Oluştur" butonuna tıklayın.</li>
-                        <li>E-posta konu başlığı ve e-posta içeriği yapay zeka terafından yoğunluğa bağlı bir hızla oluşturuluyor birkaç saniye bekleyin</li>
-                        <li>E-posta metni sizin için uygunsa "Kopyala" butonuna basarak içeriği kopyalayın ve kullanmaya başlayın</li>
+                        <li>Open the <a href="emailgpt.flerosoft.com">EmailGPT</a> web app in your favorite browser</li>
+                        <li>Select the language you want to use</li>
+                        <li>Enter your desired subject or topic in the text input box</li>
+                        <li>Click the "Generate Email" button</li>
+                        <li>Wait a few seconds while EmailGPT generates a natural-sounding email for you</li>
+                        <li>Click the "Copy to Clipboard" button to copy the email to your clipboard, or click the "Reset" button to start over</li>
                     </ol>
-                    <p className={styles.para}>Email Örnekleri ile etkileyici e-posta örnekleri elde etmek için içerik yazarı veya teknoloji uzmanı olmanıza gerek yok"." İstediğiniz konu başlığını girin ve Emailornekleri com un sizin için Open AI desteğiyle e-posta içeriği oluşturmasını bekleyin İşte bu kadar basit.</p>
+                    <p className={styles.para}>With EmailGPT, you don't need to be a writer or a tech expert to craft impressive emails that get the job done. Simply input your desired message and watch as EmailGPT generates the perfect email for you.</p>
                 </div>
             </div>
             <div className={'ads ad-desktop banner-728x90'}>
